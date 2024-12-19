7bc2d4391ed2d8c6c45558f72e52f84dd2c56c88
kholoud khaled <154567194+kholoud-khaled0@users.noreply.github.com>
2024-12-19 09:38:16 +0200

    Update Main.java

--- MasterMind/src/master/Main.java
+++ MasterMind/src/master/Main.java
@@ -1,40 +1,18 @@
 package master;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
 public class Main extends Application {
+    @Override
     public void start(Stage primaryStage) {
-GameController controller = new GameController();
-primaryStage.setTitle("Masrter Mind ⌛");
-primaryStage.setResizable(false);
-controller.startTimer();
-VBox layout = controller.createGameLayout();
- Scene scene = new Scene(layout,900,700);
- scene.getStylesheets().add(getClass().getResource("CSSMaster.css").toExternalForm());
- primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("master.png")));
- primaryStage.setScene(scene);
- primaryStage.show();
-
-
+        WelcomeScreen welcomeScreen = new WelcomeScreen();
+        welcomeScreen.display(primaryStage);
     }
-    public static void main(String[] args) {
-launch(args);
 
+    public static void main(String[] args) {
+        launch(args);
     }
 }
